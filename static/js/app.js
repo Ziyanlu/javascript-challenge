209b16028d6041fc5431a5dc1f3463bbf5af4d89
Ziyan <ziyan@northwestern.edu>
2019-11-17 22:06:01 -0600

    11/17

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+});
+
+function selectInput(exactdate) {
+    return exactdate.datetime === "1/1/2010";
+}
+
+var searchResults = data.filter(selectInput);
+console.log(searchResults);
+
+var table = d3.select("table");
+var tbody = d3.select("tbody");
+
+for (var i = 0; i < searchResults.length; i++) {
+var row = tbody.append("tr");
+row.append("td").text(searchResults[i].datetime);
+row.append("td").text(searchResults[i].city);
+row.append("td").text(searchResults[i].state);
+row.append("td").text(searchResults[i].country);
+row.append("td").text(searchResults[i].shape);
+row.append("td").text(searchResults[i].durationMinutes);
+row.append("td").text(searchResults[i].comments);
+
+}
+
+
+
