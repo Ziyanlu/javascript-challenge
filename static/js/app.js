94f91a3abc9edcd1d5408114455a396ded8f21ea
Ziyan <ziyan@northwestern.edu>
2019-11-20 22:22:36 -0600

    11-20

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,35 @@
 // from data.js
 var tableData = data;
 
+var table = d3.select("table");
+var tbody = d3.select("tbody");
+
+function buildTable(results) {
+    tbody.html("");
+    for (var i = 0; i < results.length; i++) {
+    var row = tbody.append("tr");
+    row.append("td").text(results[i].datetime);
+    row.append("td").text(results[i].city);
+    row.append("td").text(results[i].state);
+    row.append("td").text(results[i].country);
+    row.append("td").text(results[i].shape);
+    row.append("td").text(results[i].durationMinutes);
+    row.append("td").text(results[i].comments);
+    }
+}
+
 var button = d3.select("#filter-btn");
 button.on("click", function() {
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
     console.log(inputValue);
+
+    var searchResults = data.filter(ufo => ufo.datetime == inputValue);
+    console.log(searchResults);
+    buildTable(searchResults);
 });
 
-function selectInput(exactdate) {
-    return exactdate.datetime === "1/1/2010";
-}
 
-var searchResults = data.filter(selectInput);
-console.log(searchResults);
-
-var table = d3.select("table");
-var tbody = d3.select("tbody");
-
-for (var i = 0; i < searchResults.length; i++) {
-var row = tbody.append("tr");
-row.append("td").text(searchResults[i].datetime);
-row.append("td").text(searchResults[i].city);
-row.append("td").text(searchResults[i].state);
-row.append("td").text(searchResults[i].country);
-row.append("td").text(searchResults[i].shape);
-row.append("td").text(searchResults[i].durationMinutes);
-row.append("td").text(searchResults[i].comments);
-
-}
-
+buildTable(tableData);
 
 
